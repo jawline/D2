5465499d331acc3c4f3ef0fedee4e34dc73aac3e
Blake Loring <blake@parsed.uk>
2018-01-31 17:02:02 +0000

    Check 64b supportr

--- bootloader/stage2/src/main.s
+++ bootloader/stage2/src/main.s
@@ -9,8 +9,11 @@ jmp start
 
 stage_msg db "Stage 2 Starting", 13, 10, 0
 enable_a20_msg db "Enable A20", 13, 10, 0
+selector_msg db "Selector", 13, 10, 0
 gdt_msg db "GDT", 13, 10, 0
 done_msg db "Done", 13, 10, 0
+error_nolong_msg db "Error: No Long Mode", 13, 10, 0
+longmode_supported_msg db "Machine is 64bit", 13, 10, 0
 
 start:
 
@@ -31,6 +34,7 @@ start:
     ;Get ready for magical kernel land
     call enable_a20
     call load_gdt
+    call check_long_mode
 hlt:
     jmp hlt
 
@@ -64,6 +68,64 @@ enable_a20_already:
     ret
 
 ;----
+;- CPUID 64 bit check (From OSDev)
+;----
+
+check_long_mode:
+    call check_cpuid
+
+    ;Find out if the CPUID flag to check long mode exists
+    mov eax, 0x80000000    ; Set the A-register to 0x80000000.
+    cpuid                  ; CPU identification.
+    cmp eax, 0x80000001    ; Compare the A-register with 0x80000001.
+    jb hlt_nolongmode         ; It is less, there is no long mode.
+
+    ;Test the long mode CPUID instr
+    mov eax, 0x80000001    ; Set the A-register to 0x80000001.
+    cpuid                  ; CPU identification.
+    test edx, 1 << 29      ; Test if the LM-bit, which is bit 29, is set in the D-register.
+    jz hlt_nolongmode         ; They aren't, there is no long mode.
+
+    mov si, longmode_supported_msg
+    call printstr
+
+    ret
+
+check_cpuid:
+    pushfd
+    pop eax
+ 
+    ; Copy to ECX as well for comparing later on
+    mov ecx, eax
+ 
+    ; Flip the ID bit
+    xor eax, 1 << 21
+ 
+    ; Copy EAX to FLAGS via the stack
+    push eax
+    popfd
+ 
+    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
+    pushfd
+    pop eax
+ 
+    ; Restore FLAGS from the old version stored in ECX (i.e. flipping the ID bit
+    ; back if it was ever flipped).
+    push ecx
+    popfd
+ 
+    ; Compare EAX and ECX. If they are equal then that means the bit wasn't
+    ; flipped, and CPUID isn't supported.
+    xor eax, ecx
+    jz hlt_nolongmode
+    ret
+
+hlt_nolongmode:
+    mov si, error_nolong_msg
+    call printstr
+    jmp hlt
+
+;----
 ;- GDT
 ;----
 
@@ -72,14 +134,41 @@ gdt_start:
     null_limit dw 0x0
     null_base dw 0x0
     null_flags dd 0x0
+gdt_end:
+
+gdtr:
+    dw 0 ;Limit
+    dd 0 ;Base
+
+set_gdt:
+   xor   eax, eax
+   mov   ax, ds
+   shl   eax, 4
+   add   eax, gdt_start
+   mov   [gdtr + 2], eax
+   mov   eax, gdt_end
+   sub   eax, gdt_start
+   mov   [gdtr], ax
+   lgdt  [gdtr]
+   ret
 
 load_gdt:
     mov si, gdt_msg
     call printstr
 
     ;Load our dummy GDT
-    lgdt [gdt_start]
+    call set_gdt
 
+    mov si, selector_msg
+    call printstr
+
+    ;jmp 0x08:load_gdt_selector
+
+load_gdt_selector:
+
+    mov si, done_msg
+    call printstr
+ 
     ret
 
 ;-----
