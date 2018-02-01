aa90f89f02699a90db189a5dfc91ec0a86797054
Blake Loring <blake@parsed.uk>
2018-02-01 13:34:44 +0000

    Cleaning up long mode

--- bootloader/stage2/src/long.s
+++ bootloader/stage2/src/long.s
@@ -1,8 +1,15 @@
 [bits 64]
 
+screen_ptr dd 0xB8000
+screen_size dd ((80 * 25) / 4)
+
+all_white dq 0xFFFFFFFFFFFFFFFF
+all_black dq 0x0000000000000000
+
 long_entry:
     cli                           ; Clear the interrupt flag.
 
+    ;Update the stack register
     mov ax, gdt_64.data           ; Set the A-register to the data descriptor.
     mov ds, ax                    ; Set the data segment to the A-register.
     mov es, ax                    ; Set the extra segment to the A-register.
@@ -10,9 +17,16 @@ long_entry:
     mov gs, ax                    ; Set the G-segment to the A-register.
     mov ss, ax                    ; Set the stack segment to the A-register.
 
-    mov edi, 0xB8000              ; Set the destination index to 0xB8000.
-    mov rax, 0xFFFFFFFFFFFFFFFF   ; Set the A-register to 0x1F201F201F201F20.
-    mov ecx, 500                  ; Set the C-register to 500.
-    rep stosq                     ; Clear the screen.
+    mov rax, [all_black]
+    call clear_screen_64
 
+hlt_64:
+    cli
     hlt                           ; Halt the processor.
+
+;Write rax repeatedly into the screen ptr
+clear_screen_64: 
+    mov edi, [screen_ptr]              ; Point to screen memory
+    mov ecx, [screen_size]      ; Set the loop count to screen size
+    rep stosq                     ; Clear the screen.   
+    ret
