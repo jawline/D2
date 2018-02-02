669cabf684b47fbc4039b391ea948582aec3ba46
Blake Loring <blake_l@parsed.uk>
2018-02-02 15:27:57 +0000

    Update main.s

--- bootloader/stage1/src/main.s
+++ bootloader/stage1/src/main.s
@@ -63,23 +63,9 @@ load_fail:
     call printstr
 
 hlt:
-    jmp hlt
-
-
-;----
-;- Help functions
-;----
-printstr:
-   cld                    ; clear df flag - lodsb increments si
-printstr_loop:
-   lodsb                  ; load next character into al, increment si
-   or al, al              ; sets zf if al is 0x00
-   jz printstr_end
-   mov ah, 0x0E           ; teletype output (int 0x10)
-   int 0x10               ; print character
-   jmp printstr_loop
-printstr_end:
-   ret                    ; return to caller address
+    cli ;Disable interrupts
+    hlt ;Halt machine
+    jmp hlt ;if we wake up redo
 
 ;Pad to 512 bytes
 times (stage_1_size - 2) - ($ - $$) db 0x00
