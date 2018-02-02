a8098750cd64393da67b3ac3ca9c653b81056e65
Blake Loring <blake_l@parsed.uk>
2018-02-02 15:33:15 +0000

    Update main.s

--- bootloader/stage1/src/main.s
+++ bootloader/stage1/src/main.s
@@ -4,28 +4,31 @@ bits 16
 
 jmp start
 
+times 93 db 'F' ;Save some memory for the FAT master record
+
+;After the FS information we load files/data and have entry point
 %include "sizes.s"
 %include "read.s"
-
-times 93 db 'F' ;Save some memory for the FAT master record
+%include "help.s"
 
 stage_1_msg db "Stage 1 Entry", 13, 10, 0
 load_msg db "Loading...", 13, 10, 0
 is_floppy_msg db "Selected floppy disk", 13, 10, 0
 is_hdd_msg db "Selected hdd", 13, 10, 0
 load_fail_msg db "Load Error", 13, 10, 0
-done_load db "Loaded, Jumping", 13, 10, 0
 
 start:
 
+.load_segments:
     ;Clear initial state
     xor ax, ax
     mov ds, ax
     mov es, ax
 
-    mov [disk_num], dl ;Store initial disk number
-    
-    ;Video setup
+.store_disk:
+    mov [disk_num], dl
+
+.set_video_mode:
     mov ah, 0
     mov al, 2
     int 0x10
@@ -37,12 +40,14 @@ start:
     mov ah, 0x08
     int 0x10
 
-    mov si, stage_1_msg ;S1 msg
-    call printstr
-
     ;---
     ;- Load Stage 2
     ;---
+    
+.start_loading:
+
+    mov si, stage_1_msg ;S1 msg
+    call printstr
 
     ;Call the loading message
     mov si, load_msg
@@ -50,22 +55,15 @@ start:
 
     call read_hdd
     
-    ;Check if AH=1 (success)
+    ;Check if AF != 0 (success)
     and ah, ah
-    jz load_fail
+    jz .fail
 
-    mov si, done_load
-    call printstr
     jmp 0x7E00
 
-load_fail:
+.fail:
     mov si, load_fail_msg
     call printstr
-
-hlt:
-    cli ;Disable interrupts
-    hlt ;Halt machine
-    jmp hlt ;if we wake up redo
 
 ;Pad to 512 bytes
 times (stage_1_size - 2) - ($ - $$) db 0x00
