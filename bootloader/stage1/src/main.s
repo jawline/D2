f9aa391b671c92f9c32a0f62203aee09e3ef7abf
Blake Loring <blake_l@parsed.uk>
2018-02-02 14:41:57 +0000

    Update main.s

--- bootloader/stage1/src/main.s
+++ bootloader/stage1/src/main.s
@@ -78,70 +78,7 @@ hlt:
 ;- Help functions
 ;----
 
-select_ah:
-    and dl, [disk_num]
-    jz select_ah_floppy
 
-select_ah_hdd:
-    mov si, is_hdd_msg
-    call printstr
-    mov ah, 0x42
-    ret
-
-select_ah_floppy:
-    mov si, is_floppy_msg
-    call printstr
-    mov ah, 0x2
-    ret
-
-
-read_hdd:
-
-    ;Reset max retries
-    mov ah, [max_retries]
-    mov [current_retries], ah
-
-    ;Set buffer through AX register
-    mov ax, 0x0000
-    mov es, ax
-
-    mov dl, [disk_num]
-    call select_ah
-    mov [read_mode], ah
-
-.loop:
-
-    ;Check if we have hit max retries
-    dec byte [current_retries]
-    jz .fail
-
-    ;Load the disk number
-    mov dl, [disk_num]
-    mov ah, [read_mode]
-    mov al, [num_sectors]
-
-    ;Do the read
-    mov dh, 0
-    mov cl, 2
-    mov ch, 0
-    mov bx, 0x7E00
-
-    int 0x13
-
-    ;If the read failed try again
-    jc .loop
-
-    ;If sectors read != sectors asked try again
-    cmp al, [num_sectors]
-    jnz .loop 
-
-    ;Return success AH=1
-    mov ah, 1
-    ret
-
-.fail:
-    mov ah, 0
-    ret
 
 printstr:
    cld                    ; clear df flag - lodsb increments si
