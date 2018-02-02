b4c40f82e2c63c6d657be7577228347d9c20218c
Blake Loring <blake_l@parsed.uk>
2018-02-02 15:29:25 +0000

    Create help.s

---
+++ bootloader/stage1/src/help.s
@@ -0,0 +1,17 @@
+[bits 16]
+
+;----
+;- Stage 1 helper functions
+;----
+
+printstr:
+   cld                    ; clear df flag - lodsb increments si
+printstr_loop:
+   lodsb                  ; load next character into al, increment si
+   or al, al              ; sets zf if al is 0x00
+   jz printstr_end
+   mov ah, 0x0E           ; teletype output (int 0x10)
+   int 0x10               ; print character
+   jmp printstr_loop
+printstr_end:
+   ret                    ; return to caller address
