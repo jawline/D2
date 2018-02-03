3b5e8d67d0db2a7589c28c3a196c9709a0755f78
Blake Loring <blake@parsed.uk>
2018-02-03 02:17:44 +0000

    Actually write the FAT

--- img_create/src/main.c
+++ img_create/src/main.c
@@ -7,6 +7,7 @@ const size_t fat_bpp_offset = 3;
 const size_t sector_size = 512;
 const size_t num_sectors_fat = 8;
 const size_t num_sectors_root_dir = 16;
+const size_t cluster_entry_size = 2;
 
 typedef struct {
     char oem_identifier[8];
@@ -21,7 +22,7 @@ typedef struct {
     uint16_t sectors_per_track;
     uint16_t num_heads;
     uint32_t junk_2;
-    uint32_t total_sector_count_fat32;
+    uint32_t large_sector_count;
     uint16_t junk_3;
     uint8_t boot_signature;
     uint32_t volume_id;
@@ -160,24 +161,32 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    //Construct a new master record
     fat_bpp fs_info;
     memset(&fs_info, 0, sizeof(fs_info));
     strcpy(fs_info.oem_identifier, "MSWIN4.1");
     
     fs_info.bytes_per_sector = sector_size;
-    fs_info.sectors_per_cluster = 1;    
+    fs_info.sectors_per_cluster = 1; //For now? 
+    fs_info.num_fats = 2; //For some reason having 2 fats is common
+    fs_info.max_root_entries = 512 / 32; //1 sector
+    fs_info.sectors_per_fat = num_sectors_fat;
 
     uint8_t* final_data = read_bootloader(argv[1]);
     size_t final_length = sector_size;
 
     if (!final_data) {
         printf("Failed to read the bootloader\n");
+        return -1;
     }
 
     printf("Read bootloader %s\n", argv[1]);
 
     printf("Writing Stage2\n");
-    write_file(argv[2], final_data, &final_length);
+    
+    if (!write_file(argv[2], final_data, &final_length)) {
+        return -1;
+    }
 
     //Save the number of reserved sectors
     fs_info.reserved_sectors = final_length / sector_size;
@@ -186,19 +195,71 @@ int main(int argc, char** argv) {
     uint8_t* fat_1 = allocate_sectors(final_data, &final_length, num_sectors_fat);
     uint8_t* fat_2 = allocate_sectors(final_data, &final_length, num_sectors_fat);
 
+    memset(fat_1, 0xFF, num_sectors_fat * sector_size);
+    memset(fat_2, 0xFF, num_sectors_fat * sector_size);
+
     //Allocate space for the root directory
     uint8_t* root_directory = allocate_sectors(final_data, &final_length, num_sectors_root_dir);
+    size_t root_dir_pointer = 0;
 
     if (!fat_1 || !fat_2 || !root_directory) {
         printf("Failed to allocate memory for FAT tables and root directory\n");
         return -1;
     }
 
+    size_t data_segment_start = final_length;
+
+    #define TO_CLUSTER(x) (x - data_segment_start) / (sector_size * fs_info.sectors_per_cluster)
+    #define GET_CLUSTER(x) (uint16_t*)(fat_1 + (cluster_entry_size * x))
+
     //Write the files in
     for (int i = 3; i < argc - 1; i++) {
         printf("Loading %s\n", argv[i]);
+        
+        size_t start = final_length;
         write_file(argv[i], final_data, &final_length);      
+        size_t end = final_length;
+
+        char* name = argv[i];
+        char* temp = strtok(name, "/");
+
+        while (temp) {
+            name = temp;
+            temp = strtok(0, "/");
+        }
+
+        printf("Naming %s\n", name);
+    
+        fat_file_entry new_file;
+        memset(&new_file, 0, sizeof(fat_file_entry));
+        strcpy(new_file.filename, name);
+        new_file.file_size = end - start;
+        new_file.first_cluster = TO_CLUSTER(start);
+        size_t end_cluster = TO_CLUSTER(end);
+
+        printf("Decided first cluster will be %i (real offset %i)\n", new_file.first_cluster, start - data_segment_start);
+        printf("I will end at cluster %i (Real offset %i)\n", TO_CLUSTER(end), end - data_segment_start);
+
+        for (unsigned int i = new_file.first_cluster; i < end_cluster; i++) {
+            *GET_CLUSTER(i) = i + 1;
+        }
+        *GET_CLUSTER(end_cluster) = 0xFFFF;
+
+        printf("Wrote the FAT\n");
+
+        memcpy(root_directory + root_dir_pointer, &new_file, sizeof(fat_file_entry));
+        root_dir_pointer += sizeof(fat_file_entry);
     }
+
+    //Update the fs info and write it into memory
+    fs_info.sector_count = final_length / sector_size;
+    fs_info.large_sector_count = fs_info.sector_count;
+    fs_info.sectors_per_track = fs_info.sector_count;
+    fs_info.num_heads = 1;
+    fs_info.boot_signature = 0x28;
+    fs_info.volume_id = 1;
+    strcpy(fs_info.volume_label, "OSTEST");
+    strcpy(fs_info.fs_type, "FAT16");
 
     memcpy(final_data + fat_bpp_offset, &fs_info, sizeof(fs_info));
 
