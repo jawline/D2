858d7624aeb969862e55c2a34faa63527d0aeaac
Blake Loring <blake@parsed.uk>
2018-02-03 16:17:14 +0000

    Begin working out the root directory

--- bootloader/stage2/src/disk.s
+++ bootloader/stage2/src/disk.s
@@ -2,17 +2,23 @@
 ;- Tool to read the kernel from disk
 ;----
 
-%define volume_offset 43
-%define oem_offset 3
-%define oem_identifier boot_location + oem_offset
-%define volume_name boot_location + volume_offset
-
+;Offsets into  the FAT record stored within the boot sector
+%define oem_identifier boot_location + 3
+%define volume_name boot_location + 43
+%define reserved_sectors boot_location + 13
+%define sectors_per_fat boot_location + 22
+%define total_fats boot_location + 16
+%define root_dir_entries boot_location + 17
 
 %define max_retries 10
 
 disk_msg db "Loading from disk: ", 0
 space db " ", 0
 newline_16 db 13, 10, 0
+bad_read_msg db "Bad Read", 13, 10, 0
+
+ld_fat_msg db "Loaded FAT1", 13, 10, 0
+ld_dir_msg db "Loaded Root Directory", 13, 10, 0
 
 load_kernel:
 
@@ -31,17 +37,89 @@ load_kernel:
     mov si, newline_16
     call print_str_16
 
+    ;---
+    ;- Load FAT1 (The first FAT table on the disk)
+    ;---
+
+    ;Calculate the number of sectos
+    mov word ax, [sectors_per_fat]
+    mov word [num_sectors], ax
+  
+    ;Calculate the start sector
+    mov word dx, [reserved_sectors]
+    add dx, 1
+    mov word [start_sector], dx
+
+    mov word [target_location], $$ + stage_2_size
+    call read_from_disk     
+
+    and ah, ah
+    jz .fail
+
+    mov si, ld_fat_msg
+    call print_str_16
+
+    ;---
+    ;- Load Root Directory
+    ;---
+
+    ;Work out where in memory it should go
+
+    ;Multiply the size of a sector by the number of sectors in a FAT
+    mov word ax, 1
+    mov word dx, [sectors_per_fat]
+    mul dx
+
+    ;Multiply that by the number of fats
+    mov byte dx, [total_fats]
+    mul dx
+
+    mov word dx, [start_sector]
+    add ax, dx
+    mov word [start_sector], dx
+
+    ;Add this new size to the target location
+    mov word dx, [target_location]
+    add dx, ax
+    mov [target_location], dx
+
+    ;Calculate the directory size from the number of entries
+    mov word ax, [root_dir_entries]
+    mov dx, 32
+    mul dx
+    mov dx, sector_size
+    div dx
+    mov [num_sectors], ax
+
+    call read_from_disk
+    and ah, ah
+    jz .fail
+
+    mov si, ld_dir_msg
+    call print_str_16
+
+
+    mov ah, 1
+    ret
+
+.fail:
+    mov si, bad_read_msg
+    call print_str_16
+    mov ah, 0
+    ret
+
 jmp $
     ret
 
 ;---
 ;- Loading variables
 ;---
+start_sector db 0
 read_mode db 0
 disk_num db 0
 current_retries db 0
 num_sectors db 0
-target_location dd 0
+target_location dw 0
 
 select_ah:
     and dl, [disk_num]
@@ -82,7 +160,7 @@ read_from_disk:
 
     ;Do the read
     mov dh, 0
-    mov cl, 2
+    mov cl, [start_sector]
     mov ch, 0
     mov bx, [target_location]
 
